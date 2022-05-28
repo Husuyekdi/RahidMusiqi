fab72bef9b9091c2694b26d2441dee4d2f185edf
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-05-28 23:48:30 +0400

    update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -70,7 +70,7 @@ async def cbbilgi(_, query: CallbackQuery):
       [
         [
           InlineKeyboardButton(
-            "✨İstifadəçi Əmrləri" callback_data ="herkes")
+            "✨Hərkəs üçün Komutlar", callback_data ="herkes")
         ],
         [
           InlineKeyboardButton(
