df8c491c56bca33365be8c978a82acb3cee57115
Rahid2003 <104177696+Rahid2003@users.noreply.github.com>
2022-05-28 15:51:53 +0400

    update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -30,7 +30,7 @@ async def start(_, message: Message):
                         "🧩 Komutlar" , callback_data= "cbbilgi"
                     ),
                     InlineKeyboardButton(
-                        "Resmi Kanal 🇹🇷", url=f"https://t.me/Rahid_44"
+                        "Resmi Kanal 🇦🇿", url=f"https://t.me/Rahid_44"
                     )
                 ]
                 
@@ -147,7 +147,7 @@ async def cbstart(_, query: CallbackQuery):
                         "🌀 Komutlar" , callback_data= "cbbilgi"
                     ),
                     InlineKeyboardButton(
-                        "Resmi Kanal 🇹🇷", url=f"https://t.me/Rahid_44"
+                        "Resmi Kanal 🇦🇿", url=f"https://t.me/Rahid_44"
                     )
                 ]
                 
