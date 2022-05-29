2d0b4f3caa5be43e07703f665a765b92d0fac7c9
Rahid2003 <104177696+Rahid2003@users.noreply.github.com>
2022-05-29 16:51:35 +0400

    update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -109,7 +109,7 @@ async def play(_, message: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "Rahid_Music"
+        user.first_name = "Rahid_Asistan"
     usar = user
     wew = usar.id
     try:
