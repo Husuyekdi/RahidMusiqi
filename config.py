b88bf04c7f62b165061eead2bd0045a30fc8775e
Rahid2003 <104177696+Rahid2003@users.noreply.github.com>
2022-05-30 03:36:47 +0400

    update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "TaliaMüzik") 
+BOT_NAME = getenv("BOT_NAME", "Rahid_Asistan") 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME", "Rahid_Music_Bot")
